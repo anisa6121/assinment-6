5f3b963b987ef422130f777328cf6f20e7b03250
anisa6121 <you@example.com>
2022-09-03 16:37:35 +0600

    modal body complete

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const loadCategory = async () => {
-	const url = ` https://openapi.programming-hero.com/api/news/categories `;
-	const res = await fetch(url);
-	const data = await res.json();
-	// console.log(data)
-	displayCategory(data.data.news_category);
+    try {
+      const url = ` https://openapi.programming-hero.com/api/news/categories `;
+      const res = await fetch(url);
+      const data = await res.json();
+      // console.log(data)
+      displayCategory(data.data.news_category);  
+        
+    } catch (error) {
+        console.log(error);
+    }
+
 };
 
 const displayCategory = (newsId) => {
 	// console.log(newsId)
 	newsId.forEach((news) => {
-		console.log(news);
+	// console.log(news);
 
         const newsCategory = document.getElementById("news-category");
         
-        
-
-		const category = document.createElement("button");
+        const category = document.createElement("button");
 		category.classList.add("btn");
 		category.innerHTML = `
 		<button onclick="loadNewseId('${news.category_id}')" type="button" class="btn btn-warning me-2 ">${news.category_name}</button>
@@ -26,16 +30,25 @@ const displayCategory = (newsId) => {
 };
 
 const loadNewseId = async (id) => {
-	// console.log(".............", id.toString(), typeof id);
-	const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
-	const res = await fetch(url);
-	const data = await res.json();
-	// console.log(data.data);
-	displayNews(data.data);
+	
+    try {
+      const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      // console.log(data.data);
+      displayNews(data.data);  
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+    
+
 };
 
 const displayNews = (allNews) => {
-	console.log(allNews);
+    console.log(allNews);
+    
  const showAllNews = document.getElementById("allNews");
 
  showAllNews.textContent = "";
@@ -52,15 +65,13 @@ const displayNews = (allNews) => {
 	showAllNewsDiv.innerHTML = `
         
        <div class="col-md-4 mb-5">
-            <img src='${
-			showNews.thumbnail_url
-		}' class="img-fluid rounded-start" alt="...">
+        <img src='${showNews.thumbnail_url}' class="img-fluid rounded-start" alt="...">
         </div>
         <div class="col-md-8">
             <div class="card-body">
                 <h5 class="card-title fw-bold">${showNews.title}</h5>
                 <p class="card-text">${
-				showNews.details.slice(0, 200) + "...."
+				showNews.details.slice(0, 400) + "...."
 			}</p>
               
       <div class="d-flex">
@@ -78,54 +89,76 @@ const displayNews = (allNews) => {
      }</p>  </div>
 
 <div class="d-flex align-items-center mx-4  ">
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+<button onclick=" loadNewsDetail('${showNews._id}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
     Detail View
 </button>
 
 </div>
 
-
-        </div>
-
-             
-            </div>
+</div>
+</div>
       
+ </div>
 
+ `;
 
-
-
-        </div>
-
-        
-        
-        
-        `;
-
-		showAllNews.appendChild(showAllNewsDiv);
-	});
+	showAllNews.appendChild(showAllNewsDiv);
+});
 };
 
-loadNewseId();
+
+const loadNewsDetail = async (news) => {
+	
+
+    try {
+      const url = ` https://openapi.programming-hero.com/api/news/${news} `;
+      const res = await fetch(url);
+      const data = await res.json();
+      // console.log(data.data);
+      displayNewsDetails(data.data);  
+
+    } catch (error) {
+        console.log(error)
+    }
+	
+    
+
+};
+
+const displayNewsDetails = (newsDetail) => {
+    // console.log(newsDetail);
+
+    const modalTitle = document.getElementById("newsDetailModal");
+    
+     modalTitle.innerText =` ${newsDetail[0].title} `;
+
+    const newsDetails = document.getElementById("newsDetail");
+    newsDetails.innerHTML = `
+
+       <div>   <img src="${
+		newsDetail[0].author.img
+	}" alt="..." class=" img-thumbnail img-fluid" style="width: 10rem;"  > </div>
+    <p>Name: ${
+		newsDetail[0].author.name
+			? newsDetail[0].author.name
+			: "No Name Found"
+    } </p>
+    <p>date: ${newsDetail[0].author.published_date}</p>
+<p> rating: ${newsDetail[0].rating.number}</p>
+
+<p>View: ${
+		newsDetail[0]?.total_view
+			? newsDetail[0].total_view
+			: "No View Found"
+    } </p>
+    
+    `;
+    
+}
+
+// loadNewsDetail()
+
+// loadNewseId();
 
 loadCategory();
 
-// author:
-// img: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
-// name: "Jimmy Dane"
-// published_date: "2022-08-24 17:27:34"
-// [[Prototype]]: Object
-// category_id: "01"
-// details: "Wednesday, August 24, 2022 | Tag Cloud Tags: Biden, EU, Euro, Europe, Joe Biden, Military, News, Russia, Security, UK, Ukraine, United States, Worthy News (Worthy News) – U.S. President Joe Biden has announced nearly $3 billion in new U.S. military aid for Kyiv as Ukraine marked its independence day six months after Russia invaded the country.'The United States of America is committed to supporting the people of Ukraine as they continue the fight to defend their sovereignty. As part of that commitment, I am proud to announce our biggest tranche of security assistance to date: approximately $2."
-// image_url: "https://i.ibb.co/M23fhxm/unsplash-Eh-Tc-C9s-YXsw.png"
-// others_info:
-// is_todays_pick: false
-// is_trending: true
-// [[Prototype]]: Object
-// rating:
-// badge: "Excellent"
-// number: 4.5
-// [[Prototype]]: Object
-// thumbnail_url: "https://i.ibb.co/QnwC4sG/unsplash-Eh-Tc-C9s-YXsw-11.png"
-// title: "Biden Pledges Nearly $3 Billion To Ukraine In Largest U.S. Military Aid Package Yet"
-// total_view: 488
-// _id: "0282e0e58a5c404fbd15261f11c2ab6a"
